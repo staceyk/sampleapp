56e73943dcd9b25b2abbda89f66462b35ee3c439
Stacey King <stacey@netengine.com.au>
2012-09-07 15:48:42 +1000

    initial commit v2

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-Sampleapp::Application.config.secret_token = 'e41bc4064c85ea173023ad09a71afbad03c67fa960a7d9eb8b97970d0833a1236e20a8d2c09cd8ad4b1df36d37d350c82f8e3886f2308c5c24e607a62197e61b'
+Sampleapp::Application.config.secret_token = 'adcdd61d7354873fca1b6c4ac4ba995c3b953b8b17bf5bdf92c13f72c24fb46ddf39990bf5c07c5059928929c3f7d265b56d96f9429d492d1b4900181d8cf2e7'
